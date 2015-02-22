d85f49a69144fa3d933497a3a37695d06fc15793
Yasyf Mohamedali <yasyfm@gmail.com>
2015-02-22 07:42:51 -0500

    Add screenshots to github comments

--- app/models/feature_branch.rb
+++ app/models/feature_branch.rb
@@ -77,7 +77,9 @@ class FeatureBranch < ActiveRecord::Base
 
   def comment
     if pr && port && ENV['HOOK_HOST']
-      text = "## Branch Staged\nThis feature branch has been staged [here](http://#{ENV['HOOK_HOST']}:#{port}) as of #{sha}."
+      url = "http://#{ENV['HOOK_HOST']}:#{port}"
+      text = """## Branch Staged\nThis feature branch has been staged [here](#{url}) as of #{sha}.\n
+      ![](http://api.page2images.com/directlink?p2i_url=#{url}&p2i_key=f5e1287dd70a5b6f)"""
       if (old = old_comment)
         client.update_comment repo.full_name, old[:id], text
       else
