3c178f0de9b62e67dbef5ac909ec3bdd0fe7b9f4
Yasyf Mohamedali <yasyfm@gmail.com>
2015-02-22 06:03:08 -0500

    Add delete_hook to before_destroy

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,6 +1,7 @@
 class Repo < ActiveRecord::Base
-  has_many :feature_branches
+  has_many :feature_branches, dependent: destroy
   after_save :create_hook
+  before_destroy :delete_hook
 
   store :secrets, coder: JSON
 
@@ -9,14 +10,26 @@ class Repo < ActiveRecord::Base
   end
 
   def client
-    @client ||= Octokit::Client.new(:access_token => Rails.application.secrets.gh_token)
+    @client ||= Octokit::Client.new(:access_token => ENV['GH_TOKEN'] || Rails.application.secrets.gh_token)
+  end
+
+  def hook
+    ENV['HOOK_HOST'] && client.hooks(full_name).find { |hook| hook[:config][:url].include? ENV['HOOK_HOST'] }
   end
 
   private
 
   def create_hook
-    config = {url: "http://#{ENV['HOOK_HOST']}/webhook", :content_type => 'json'}
-    options = {events: ['pull_request', 'delete'], active: true}
-    client.create_hook full_name, 'web', config, options
+    if ENV['HOOK_HOST']
+      config = {url: "http://#{ENV['HOOK_HOST']}/webhook", :content_type => 'json'}
+      options = {events: ['pull_request', 'delete'], active: true}
+      client.create_hook full_name, 'web', config, options
+    end
+  end
+
+  def delete_hook
+    if hook.present?
+      client.remove_hook full_name, hook[:id]
+    end
   end
 end
