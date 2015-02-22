4c0aac250de0726936569755299c90742a358d92
Yasyf Mohamedali <yasyfm@gmail.com>
2015-02-22 04:42:22 -0500

    Make client public

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -8,15 +8,15 @@ class Repo < ActiveRecord::Base
     "#{user}/#{name}"
   end
 
+  def client
+    @client ||= Octokit::Client.new(:access_token => Rails.application.secrets.gh_token)
+  end
+
   private
 
   def create_hook
     config = {url: "http://#{ENV['HOOK_HOST']}/webhook", :content_type => 'json'}
     options = {events: ['pull_request', 'delete'], active: true}
     client.create_hook full_name, 'docker', config, options
-  end
-
-  def client
-    @client ||= Octokit::Client.new(:access_token => Rails.application.secrets.gh_token)
   end
 end
